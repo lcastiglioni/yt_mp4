7830974fef1dcb95c4edf5fc26d8ec5a6041cb9b
Lucas Castiglioni <castiglionidamian@gmail.com>
2022-08-16 18:51:28 -0300

    change str_input

--- index.py
+++ index.py
@@ -1,15 +1,13 @@
 from pytube import YouTube
 
 
-link = "https://www.youtube.com/watch?v=psQk3Z_bmUc"
+link = str(input())
 
 yt = YouTube(link)
 
 
-formato = "mp3"
-calidad = "720p"
 resoluciones = yt.streams.order_by('resolution')
 resoluciones2 = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution')
 resolucion = resoluciones2.first()
 video = resolucion.download()
-print("hola")
+print("descarga finalizada")
